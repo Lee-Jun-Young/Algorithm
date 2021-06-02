6786366b25e00898cdab7e0c20ae42ef6fa1f2b3
Lee-Jun-Young <leejy8506@naver.com>
2021-06-02 19:33:57 +0900

    스택_균형잡힌 세상 풀이

---
+++ baekjoon/_4949.java
@@ -0,0 +1,54 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
+public class _4949 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		while(true) {
+			String str = br.readLine();
+			
+			if(str.equals(".")) {
+				break;
+			}
+			sb.append(isChecked(str)).append("\n");
+		}
+		System.out.println(sb);
+	}
+	
+	public static String isChecked(String str) {
+		
+		Stack<Character> stack = new Stack<Character>();
+				
+		for(int i = 0; i < str.length(); i++){
+			char c = str.charAt(i);
+			
+			if(c == '(' || c == '[') {
+				stack.push(c);
+			}
+			else if(c == ')') {
+				if(stack.empty() || stack.peek() != '(') {
+					return "no";
+				}
+				else stack.pop();
+			}
+			else if(c == ']') {
+				if(stack.empty() || stack.peek() != '[') {
+					return "no";
+				}
+				else stack.pop();
+			}
+		}
+		
+		if(stack.isEmpty()) 
+			return "yes";
+		else 
+			return "no";
+		
+	}
+}
