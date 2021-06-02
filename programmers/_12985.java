604140786cfa03066041881c066f7ad6650ec3fc
Lee-Jun-Young <leejy8506@naver.com>
2021-06-02 15:15:14 +0900

    2017_팀스타운_예상 대진표 풀이

---
+++ programmers/_12985.java
@@ -0,0 +1,25 @@
+package programmers;
+
+public class _12985 {
+
+	public static void main(String[] args) {
+		
+		int N = 8;
+		int A = 4;
+		int B = 7;
+		
+		System.out.println(solution(N,A,B));
+	}
+
+	public static int solution(int n, int a, int b) {
+		int answer = 0;
+		
+		while(a != b) {
+			a = (a+1) / 2;
+			b = (b+1) / 2;
+			answer++;
+		}
+		
+		return answer;
+	}
+}
