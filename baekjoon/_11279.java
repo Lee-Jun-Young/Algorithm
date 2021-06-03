7ed5cee2c24b97a60ed5862ed394845ba23e24e0
Lee-Jun-Young <leejy8506@naver.com>
2021-06-03 13:53:45 +0900

    우선순위 큐_최대 힙 풀이

---
+++ baekjoon/_11279.java
@@ -0,0 +1,37 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Collections;
+import java.util.PriorityQueue;
+
+public class _11279 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		
+		PriorityQueue<Integer> heap = new PriorityQueue<Integer>(Collections.reverseOrder());
+		
+		int N = Integer.parseInt(br.readLine());
+		
+		while(N > 0) {
+			int temp = Integer.parseInt(br.readLine());
+			
+			if(temp == 0) {
+				if(heap.isEmpty())
+					sb.append("0").append("\n");
+				else
+					sb.append(heap.poll()).append("\n");
+			}
+			else {
+				heap.add(temp);
+			}
+			N--;
+		}
+		
+		System.out.println(sb);
+	}
+
+}
