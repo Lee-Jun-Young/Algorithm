1619ddd6f5b3a259261e0f4527ae140ab4181950
Lee-Jun-Young <leejy8506@naver.com>
2021-06-15 11:40:10 +0900

    수학_이항 계수1 풀이

---
+++ baekjoon/_11050.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11050 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine(), " ");
+		
+		int N = Integer.parseInt(st.nextToken());
+		int M = Integer.parseInt(st.nextToken());
+		
+		System.out.println(factorial(N) / (factorial(M) * factorial(N-M)));
+		System.out.println(factorial(M));
+		System.out.println(factorial(N-M));
+	}
+	
+	public static int factorial(int num) {
+		int result = 1;
+		
+		while(num > 0) {
+			result *= num;
+			num--;
+		}
+		
+		return result;
+	}
+}
+
+/*
+	n!
+	r!(n-r)!
+
+	5
+	2
+
+*/
