6dc5001ec59fc8a8cb4b5c7d6bab170b140afcac
Lee-Jun-Young <leejy8506@naver.com>
2021-07-05 11:58:47 +0900

    스택/큐_다리를 지나는 트럭 풀이

---
+++ programmers/_42583.java
@@ -0,0 +1,48 @@
+package programmers;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class _42583 {
+
+	public static void main(String[] args) {
+		int bridge_length = 2;
+		int weight = 10;
+		int[] truck_weights = { 7, 4, 5, 6 };
+
+		System.out.println(solution(bridge_length, weight, truck_weights));
+
+	}
+
+	public static int solution(int bridge_length, int weight, int[] truck_weights) {
+		int answer = 0;
+		Queue<Integer> queue = new LinkedList<>();
+
+		int sum = 0;
+		for (int w : truck_weights) {
+
+			while (true) {
+				if (queue.isEmpty()) {
+					queue.offer(w);
+					sum += w;
+					answer++;
+					break;
+				} else if (queue.size() == bridge_length) {
+					sum -= queue.poll();
+				} else {
+					if (sum + w <= weight) {
+						queue.offer(w);
+						sum += w;
+						answer++;
+						break;
+					} else {
+						queue.offer(0);
+						answer++;
+					}
+				}
+			}
+		}
+
+		return answer + bridge_length;
+	}
+}
