4bba028ce964cebdc1a81c88e50e9501ba236965
Lee-Jun-Young <leejy8506@naver.com>
2021-07-09 13:01:03 +0900

    Summer/Winter_방문 길이 풀이

--- programmers/_49994.java
+++ programmers/_49994.java
@@ -25,49 +25,37 @@ public class _49994 {
 			switch (temp) {
 
 			case 'U':
-				if (y_location == 5)
-					break;
-				else {
+				if (y_location != 5) {
 					y_location += 1;
-					if (!set.contains(str + " " + x_location + " " + y_location)
-							|| !set.contains(x_location + " " + y_location + " " + str))
-						set.add(x_location + " " + y_location + " " + str);
+					set.add(str + " " + x_location + " " + y_location);
+					set.add(x_location + " " + y_location + " " + str);
 					break;
 				}
 			case 'D':
-				if (y_location == -5)
-					break;
-				else {
+				if (y_location != -5) {
 					y_location -= 1;
-					if (!set.contains(str + " " + x_location + " " + y_location)
-							|| !set.contains(x_location + " " + y_location + " " + str))
-						set.add(x_location + " " + y_location + " " + str);
+					set.add(str + " " + x_location + " " + y_location);
+					set.add(x_location + " " + y_location + " " + str);
 					break;
 				}
 			case 'R':
-				if (x_location == 5)
-					break;
-				else {
+				if (x_location != 5) {
 					x_location += 1;
-					if (!set.contains(str + " " + x_location + " " + y_location)
-							|| !set.contains(x_location + " " + y_location + " " + str))
-						set.add(x_location + " " + y_location + " " + str);
+					set.add(str + " " + x_location + " " + y_location);
+					set.add(x_location + " " + y_location + " " + str);
 					break;
 				}
 			case 'L':
-				if (x_location == -5)
-					break;
-				else {
+				if (x_location != -5) {
 					x_location -= 1;
-					if (!set.contains(str + " " + x_location + " " + y_location)
-							|| !set.contains(x_location + " " + y_location + " " + str))
-						set.add(x_location + " " + y_location + " " + str);
+					set.add(str + " " + x_location + " " + y_location);
+					set.add(x_location + " " + y_location + " " + str);
 					break;
 				}
 			}
 		}
 
-		answer += set.size();
+		answer += set.size() / 2;
 
 		return answer;
 	}
