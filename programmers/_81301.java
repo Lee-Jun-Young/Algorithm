cb8c84d409d43d0f80e9361e658c8c16fea9b5e5
Lee-Jun-Young <leejy8506@naver.com>
2021-07-16 22:06:00 +0900

    2021 카카오_숫자 문자열과 영단어 풀이 수정

--- programmers/_81301.java
+++ programmers/_81301.java
@@ -1,7 +1,5 @@
 package programmers;
 
-import java.util.Arrays;
-
 public class _81301 {
 
 	public static void main(String[] args) {
@@ -14,42 +12,15 @@ public class _81301 {
 	public static int solution(String s) {
 		int answer = 0;
 
-		while (!isNumber(s)) {
-			if (s.contains("one"))
-				s = s.replaceAll("one", "1");
-			else if (s.contains("two"))
-				s = s.replaceAll("two", "2");
-			else if (s.contains("three"))
-				s = s.replaceAll("three", "3");
-			else if (s.contains("four"))
-				s = s.replaceAll("four", "4");
-			else if (s.contains("five"))
-				s = s.replaceAll("five", "5");
-			else if (s.contains("six"))
-				s = s.replaceAll("six", "6");
-			else if (s.contains("seven"))
-				s = s.replaceAll("seven", "7");
-			else if (s.contains("eight"))
-				s = s.replaceAll("eight", "8");
-			else if (s.contains("nine"))
-				s = s.replaceAll("nine", "9");
-			else
-				s = s.replaceAll("zero", "0");
+		String[] arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
 
+		for (int i = 0; i < 10; i++) {
+			s = s.replaceAll(arr[i], Integer.toString(i));
 		}
 
 		answer = Integer.parseInt(s);
 
 		return answer;
-	}
-
-	public static boolean isNumber(String s) {
-		try {
-			Double.parseDouble(s);
-			return true;
-		} catch (NumberFormatException e) {
-			return false;
-		}
 	}
 
 }
