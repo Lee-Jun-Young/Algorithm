cfe58c2efe0909953ab222d2a15085282746713c
Lee-Jun-Young <leejy8506@naver.com>
2021-09-30 15:57:05 +0900

    프로그래머스 정수 삼각형 풀이 수정

--- programmers/_43105.java
+++ programmers/_43105.java
@@ -9,24 +9,19 @@ public class _43105 {
 
 	public static int solution(int[][] triangle) {
 		int answer = 0;
-		int[][] dp = new int[501][501];
-		dp[0][0] = triangle[0][0];
 		
 		for(int i = 1; i < triangle.length; i++) {
-			for(int j = 0; j <= i; j++) {
-				if(j == 0)
-					dp[i][j] = triangle[i][j] + dp[i-1][j];
-				else if(j == i)
-					dp[i][j] = triangle[i][j] + dp[i-1][j-1];
-				else
-					dp[i][j] = triangle[i][j] + Math.max(dp[i-1][j], dp[i-1][j-1]);
+			triangle[i][0] += triangle[i-1][0]; 
+			triangle[i][i] += triangle[i-1][i-1];
+			for(int j = 1; j < i; j++) {
+				triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
 			}
 		}
 		
-		answer = dp[triangle.length-1][0];
+		answer = triangle[triangle.length-1][0];
 		
 		for(int i = 1; i < triangle.length; i++) {
-			answer = Math.max(answer, dp[triangle.length-1][i]);
+			answer = Math.max(answer, triangle[triangle.length-1][i]);
 		}
 		
 		return answer;
