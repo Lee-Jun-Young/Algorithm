8f6e632113335b8ba83f8ec1a9bd6ceb2c38d772
Lee-Jun-Young <leejy8506@naver.com>
2021-10-01 13:56:09 +0900

    dp_가장 긴 감소하는 부분 수열 풀이

---
+++ baekjoon/_11722.java
@@ -0,0 +1,43 @@
+package baekjoon;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _11722 {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st;
+        
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        int[] dp = new int[N];
+        
+        st = new StringTokenizer(br.readLine());
+        for(int i = 0; i < N; i++) {
+        	arr[i] = Integer.parseInt(st.nextToken());
+        }
+        
+        dp[0] = 1;
+        
+        for(int i = 1; i < N; i++) {
+        	dp[i] = 1;
+        	for(int j = 0; j < i; j++) {
+            	if(arr[i] < arr[j] && dp[j] >= dp[i]) 
+            		dp[i] = dp[j] + 1;
+        	}
+        }
+        
+        int result = 0;
+        
+        for(int i = 0; i < N; i++) {
+        	result = Math.max(result, dp[i]);
+        }
+        
+        System.out.println(result);
+        	
+	}
+}
