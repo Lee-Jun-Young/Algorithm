60eaf1a36132710f82babdffd550c84b0b92f3dd
Lee-Jun-Young <leejy8506@naver.com>
2021-12-29 16:25:30 +0900

    이중우선순위큐 복습

--- programmers/_42628.java
+++ programmers/_42628.java
@@ -7,46 +7,43 @@ import java.util.PriorityQueue;
 public class _42628 {
 
 	public static void main(String[] args) {
-		String[] operations = {"I 7","I 5","I -5","D -1"};
+		String[] operations = { "I 16", "D 1" };
 
 		System.out.println(Arrays.toString(solution(operations)));
 	}
 
 	public static int[] solution(String[] operations) {
-        int[] answer = {0,0};
-        
-        PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
-        
-        for(String operation : operations) {
-        	String[] temp = operation.split(" ");
- 	
-        	if(temp[0].equals("I")) {
-        		minHeap.add(Integer.parseInt(temp[1]));
-        		maxHeap.add(Integer.parseInt(temp[1]));
-        	}
-        	
-        	if(temp[0].equals("D")) {
-        		if(!minHeap.isEmpty()) {
-        			if(Integer.parseInt(temp[1]) > 0) {
-        				int max = maxHeap.peek();
-        				minHeap.remove(max);
-        				maxHeap.remove(max);
-        			}
-        			else {
-        				int min = minHeap.peek();
-        				minHeap.remove(min);
-        				maxHeap.remove(min);
-        			}
-        		}	
-        	}
-        }
+		int[] answer = { 0, 0 };
 
-        if(!minHeap.isEmpty()) {
-        	answer[0] = maxHeap.poll();
-        	answer[1] = minHeap.poll();
-        }
+		PriorityQueue<Integer> minHeap = new PriorityQueue<Integer>();
+		PriorityQueue<Integer> maxHeap = new PriorityQueue<Integer>(Collections.reverseOrder());
 
-        return answer;
-    }
+		for (String str : operations) {
+			String[] temp = str.split(" ");
+
+			if (temp[0].equals("I")) {
+				minHeap.add(Integer.parseInt(temp[1]));
+				maxHeap.add(Integer.parseInt(temp[1]));
+			} else {
+				if (!minHeap.isEmpty()) {
+					if (Integer.parseInt(temp[1]) < 0) {
+						int min = minHeap.peek();
+						minHeap.remove(min);
+						maxHeap.remove(min);
+					} else {
+						int max = maxHeap.peek();
+						minHeap.remove(max);
+						maxHeap.remove(max);
+					}
+				}
+			}
+		}
+
+		if (!minHeap.isEmpty()) {
+			answer[0] = maxHeap.poll();
+			answer[1] = minHeap.poll();
+		}
+
+		return answer;
+	}
 }
