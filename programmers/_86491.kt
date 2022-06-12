f83dc5273e27b5a12f988a9a7790ea73c117114a
Lee-Jun-Young <leejy8506@naver.com>
2022-06-12 21:42:36 +0900

    최소직사각형 풀이

--- programmers/_86491.kt
+++ programmers/_86491.kt
@@ -1,10 +1,13 @@
-class Solution {
+package programmers
+
+class _86491 {
+
     fun solution(sizes: Array<IntArray>): Int {
         // max : 가로, 세로 길이 중 길이가 긴 것중에 최대 값
         var max = 0
         // min : 가로, 세로 길이 중 길이가 짧은 것 중에 최대 값
         var min = 0
-        
+
         sizes.forEach{
             max = maxOf(max, it[0], it[1])
             if(it[0] < it[1]){
@@ -13,7 +16,7 @@ class Solution {
                 min = Math.max(min, it[1])
             }
         }
-        
+
         return max * min
     }
 }
